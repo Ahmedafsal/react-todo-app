8f352e8707d2c2001c62f56309e840d7a16b6292
Ahmedafsal <ahmedafsal39@gmail.com>
2021-06-14 20:52:03 +0530

    small fixes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import React, { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
- return(
+
+  return(
    <div className="App">
       <div className="container">
+        
         <div className="card input-window">
           <h3>MyTodo-App</h3>
           <p><i>Hello there, It's <span style={{color: 'white'}}>
             "Monday"</span>. Are you ready?</i></p>
           <div>
-            <input type="text" />
-            <span><button>+</button></span>
+            <input  type="text" />
+            <span>
+              <button>+</button>
+            </span>
           </div>
         </div>
+
         <div className="output-window">
-         <h1>Some content</h1>
+          <div className="card output-card complted-window">completed</div>
+          <div className="card output-card active-window">active</div>
+          <div className="card output-card deleted-window">deleted</div>
         </div>
+
       </div>
    </div>
  )
