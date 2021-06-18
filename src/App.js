8c7808c6f0f249d523b6e16116cd0c5531232962
Ahmedafsal <ahmedafsal39@gmail.com>
2021-06-19 00:32:39 +0530

    first production

--- src/App.js
+++ src/App.js
@@ -7,19 +7,21 @@ function App() {
   const [todos, setTodos] = useState([])
   const [deletedTodos, setDeletedTodos] = useState([])
   const today = new Date();
-  const displayDate = today.getDay()+"/"+today.getMonth()+"/"+today.getFullYear()
+  const displayDate = ((today.getDay() < 10)?"0":"") + today.getDay() + "/" + 
+                      ((today.getMonth() < 10)?"0":"") + today.getMonth() + "/" +
+                      ((today.getFullYear() < 10)?"0":"") + today.getFullYear()
 
   useEffect(() => {
     const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
     if(storedTodos) {
-      setTodos(storedTodos)
-      //setDeletedTodos(storedTodos.deletedTodos)
+      setTodos(storedTodos.todos)
+      setDeletedTodos(storedTodos.deletedTodos)
     }
   }, []) 
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({todos, deletedTodos}))
+  }, [todos, deletedTodos])
 
   function handleButtonClick(e) {
     const text = todoTextRef.current.value
@@ -32,7 +34,12 @@ function App() {
   }
 
   function currentTime() {
-    return(today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()
+    const hours = today.getHours() 
+    const hourValue = (hours < 10) & (hours > 0)
+    return(displayDate + ",  " +
+     ( hourValue ?"0":"") + (today.getHours() === 0 ? 12 : (today.getHours() % 12))+ ":" +
+     ((today.getMinutes() < 10)?"0":"") + today.getMinutes() + " "+
+     ((today.getHours() >= 12) ? "PM" : "AM")
     )
   }
     
@@ -54,7 +61,7 @@ function App() {
         </div>
 
         <div className="view-container">    
-         
+            {/* Active container */}
             <div className="view-section">
               <h2 className="view-section-header">Active</h2>
               {todos.map(todo=> {
@@ -62,36 +69,39 @@ function App() {
                   return(
                   <div key={todo.id} className="view-card">
                     <input type="checkbox" checked={todo.status} onChange={(e)=> {
-                      window.alert("Task Complete")
-                      setTodos(todos.filter(item=>{
-                        if (item.id === todo.id) {
-                          item.status = e.target.checked
-                        }
-                        return item
-                      }))
+                      const confirmation = window.confirm("A: task completed \nB: Really...?")
+                      if (confirmation) {
+                        setTodos(todos.filter(item=>{
+                          if (item.id === todo.id) {
+                            item.status = e.target.checked
+                            item.time = currentTime()
+                          }
+                          return item
+                        }))
                       console.log(todo)
+                      }    
                     }
                     }/>
                     <p>{todo.text}</p>
                     <p className="time">
-                      <span style={{color: 'maroon', fontSize: '1em'}}>created at </span>
+                      <span style={{color: 'maroon', fontSize: '1em'}}>created on : </span>
                       {todo.time}
                     </p>
                     <button className="rmv-btn" onClick={(e) => {
-                    //setDeletedTodos([...deletedTodos, todo])
-                    console.log("deleted todos")
-                    console.log(deletedTodos)
+                    setDeletedTodos([...deletedTodos, todo])
                     setTodos(todos.filter(item=>{
                       if (item.id !== todo.id) {
                         return item
                       }
                       return null
                     }))
-                    console.log(todo)
                   }
                   }>&#10006;</button>
                   </div>
                 )  
+                }
+                else {
+                  return null
                 }
               })
               }
@@ -104,27 +114,50 @@ function App() {
               if (todo.status){
                 return(
                   <div key={todo.id} className="view-card">
-                    <input type="checkbox" checked={todo.status} onChange={(e)=> {
-                      setTodos(todos.filter(item=>{
-                        if (item.id === todo.id) {
-                          item.status = e.target.checked
-                        }
-                        return item
-                      }))
-                    }
-                    }/>
                     <p style={
                       {color: "grey"}
                     }
                     >{todo.text}</p>
-                    <p className="time">completed at "----"</p>
+                    <p className="time">completed on : {todo.time}</p>
                   </div>
                 )  
+              }else {
+                return null
               }
+
             })
             }
           </div>
           {/*Deleted todos are here*/}
+
+          <div className="view-section">
+            <h2 className="view-section-header">Deleted</h2>
+            {deletedTodos.map(todo=> {
+              console.log("deleted todo", todo)
+                return(
+                  <div key={todo.id} className="view-card view-card-deleted">
+                    <p className="restore-todo" style={{color: "#80FF00"}} 
+                      onClick={(e) =>{
+                       setTodos([...todos, todo])
+                       setDeletedTodos(deletedTodos.filter(item=>{
+                        if (item.id !== todo.id) {
+                          return item
+                        }
+                        return null
+                      }))
+                      }
+                      }
+                    >&#x27F2;</p>
+                    <p style={
+                      {color: "white"}
+                    }
+                    >{todo.text}</p>
+                    <p className="time ">deleted on : {todo.time}</p>
+                  </div>
+                )  
+            })
+            }
+          </div>
           
 
         </div>
