f08d7634025541db3251ecabe1f87e62a07484ba
Ahmedafsal <ahmedafsal39@gmail.com>
2021-06-19 21:51:50 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ const LOCAL_STORAGE_KEY = 'todoApp.todo'
 function App() {
   const todoTextRef = useRef()
   const [todos, setTodos] = useState([])
+  const [showCompleted, setShowCompleted] = useState(true)
+  const [showDeleted, setShowDeleted] = useState(true)
+
   const [deletedTodos, setDeletedTodos] = useState([])
   const today = new Date();
   const displayDate = ((today.getDay() < 10)?"0":"") + today.getDay() + "/" + 
@@ -42,13 +45,21 @@ function App() {
      ((today.getHours() >= 12) ? "PM" : "AM")
     )
   }
+
+  function handleFreshPageClick() {
+    const confirmation = window.confirm("Click 'OK' if you really need a fresh list")
+    if (confirmation) {
+    localStorage.removeItem(LOCAL_STORAGE_KEY)
+      setTodos([])
+      setDeletedTodos([])
+    }
+  }
     
-  
   return (
     <div className="app">
       <div className="container">
         <div className="add-item">
-          <h1 className="app-header">Todo-App</h1>
+          <h1 className="app-header">TODO APP</h1>
           <p className="greetings">Hey Buddy, it's
             <span style={{color: 'white'}}> {displayDate}</span>
           </p>
@@ -58,10 +69,41 @@ function App() {
                   onClick={ handleButtonClick }
             >Add</button>
           </div>
+          <button className="fresh-btn"onClick={handleFreshPageClick}>
+            Need a fresh List &#128214;
+          </button>
         </div>
 
         <div className="view-container">    
-            {/* Active container */}
+            
+{/*Completed todos here*/}
+          <div className="view-section">
+            <h2 className="view-section-header">Complete
+              <button onClick={()=> setShowCompleted(!showCompleted)}> &#128065;</button>
+            </h2>
+            {  
+              showCompleted ?
+                todos.map(todo=> {
+                  if (todo.status){
+                    return(
+                      <div key={todo.id} className="view-card">
+                        <p style={
+                          {color: "grey"}
+                        }
+                        >{todo.text}</p>
+                        <p className="time">completed on : {todo.time}</p>
+                      </div>
+                    )  
+                  }else {
+                    return null
+                  }
+
+                })
+              : ""
+            }
+          </div>
+
+{/* Active Todos here */}
             <div className="view-section">
               <h2 className="view-section-header">Active</h2>
               {todos.map(todo=> {
@@ -69,7 +111,7 @@ function App() {
                   return(
                   <div key={todo.id} className="view-card">
                     <input type="checkbox" checked={todo.status} onChange={(e)=> {
-                      const confirmation = window.confirm("A: task completed \nB: Really...?")
+                      const confirmation = window.confirm("Press 'Ok' if you complete it.")
                       if (confirmation) {
                         setTodos(todos.filter(item=>{
                           if (item.id === todo.id) {
@@ -84,8 +126,7 @@ function App() {
                     }/>
                     <p>{todo.text}</p>
                     <p className="time">
-                      <span style={{color: 'maroon', fontSize: '1em'}}>created on : </span>
-                      {todo.time}
+                      created on : {todo.time}
                     </p>
                     <button className="rmv-btn" onClick={(e) => {
                     setDeletedTodos([...deletedTodos, todo])
@@ -106,56 +147,38 @@ function App() {
               })
               }
             </div>
-           
-          {/*Completed todos here*/}
+{/*Deleted todos are here*/}
           <div className="view-section">
-            <h2 className="view-section-header">Completed</h2>
-            {todos.map(todo=> {
-              if (todo.status){
-                return(
-                  <div key={todo.id} className="view-card">
-                    <p style={
-                      {color: "grey"}
-                    }
-                    >{todo.text}</p>
-                    <p className="time">completed on : {todo.time}</p>
-                  </div>
-                )  
-              }else {
-                return null
-              }
-
-            })
-            }
-          </div>
-          {/*Deleted todos are here*/}
-
-          <div className="view-section">
-            <h2 className="view-section-header">Deleted</h2>
-            {deletedTodos.map(todo=> {
-              console.log("deleted todo", todo)
-                return(
-                  <div key={todo.id} className="view-card view-card-deleted">
-                    <p className="restore-todo" style={{color: "#80FF00"}} 
-                      onClick={(e) =>{
-                       setTodos([...todos, todo])
-                       setDeletedTodos(deletedTodos.filter(item=>{
-                        if (item.id !== todo.id) {
-                          return item
+            <h2 className="view-section-header">Delete
+              <button onClick={()=> setShowDeleted(!showDeleted)}> &#128065;</button>
+            </h2>
+            
+            { showDeleted ?
+                deletedTodos.map(todo=> {
+                  console.log("deleted todo", todo)
+                    return(
+                      <div key={todo.id} className="view-card view-card-deleted">
+                        <p className="restore-todo" style={{color: "#80FF00"}} 
+                          onClick={(e) =>{
+                          setTodos([...todos, todo])
+                          setDeletedTodos(deletedTodos.filter(item=>{
+                            if (item.id !== todo.id) {
+                              return item
+                            }
+                            return null
+                          }))
+                          }
+                          }
+                        >&#x27F2;</p>
+                        <p style={
+                          {color: "white"}
                         }
-                        return null
-                      }))
-                      }
-                      }
-                    >&#x27F2;</p>
-                    <p style={
-                      {color: "white"}
-                    }
-                    >{todo.text}</p>
-                    <p className="time ">deleted on : {todo.time}</p>
-                  </div>
-                )  
-            })
+                        >{todo.text}</p>
+                        <p className="time ">deleted on : {todo.time}</p>
+                      </div>
+                    )  
+                })
+              : ""  
             }
           </div>
           
